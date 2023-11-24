05ba8f60a5c9789f9c63e3bed0d84003e962cbbf
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2023-11-24 21:05:54 +0000

    task 0-2

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,8 @@
+# kills a process named killmenow
+
+exec { 'killmenow_process':
+	command => 'pkill -f killmenow',
+	path => '/usr/bin/',
+	refreshonly => 'true',
+	onlyif => 'pgrep -f killmenow',
+}
