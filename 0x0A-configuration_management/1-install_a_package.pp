3986603cca6d4902ad8b3ba958a8fae1d1da2271
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2023-11-25 11:59:24 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,10 @@
 # installing flask using pip3
-
 package { 'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3', 
-  }
+}
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  }
+}
