3ea97838a0be40d61ae91939cd695bc15ce2318f
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2023-11-29 18:04:49 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,23 +1,13 @@
 # Puppet to make changes to a configuration file
 
-exec { 'make ssh_config writable':
-  command     => '/usr/bin/chmod u+w /etc/ssh/ssh_config',
-  refreshonly => true,
-}
-
 file_line { 'Turn off passwd auth':
+  ensure  => present,
   path    => '/etc/ssh/ssh_config',
   line    => 'PasswordAuthentication no',
-  match   => '^#?\s*PasswordAuthentication',
-  require => Exec['make ssh_config writable'],
 }
 
 file_line { 'Declare identity file':
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
-  match  => '^#?\s*IdentityFile',
-}
-
-exec { 'make ssh_config readonly':
-  command => '/usr/bin/chmod u-w /etc/ssh/ssh_config',
 }
