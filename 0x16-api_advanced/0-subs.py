3e8a19536820a36184eacacb31648f93737ef398
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-01-17 10:45:50 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 def number_of_subscribers(subreddit):
     """queries the Reddit API and returns the number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    headers = {'User-Agent': 'My-Agent'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
