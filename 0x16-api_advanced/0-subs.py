738d3d14533655a966f3ced927345ce92b9320a0
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-01-17 10:46:58 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'My-Agent'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    if response.status_code >= 300:
         return 0
     data = response.json()
     return data.get('data').get('subscribers')
