2f7b650f88d6a389ef23f9cadc5acf457f65384c
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-01-17 10:52:22 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,14 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """queries the Reddit API and returns the number of subscribers"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'My-Agent'}
+    """
+    Returns the number of subscriber a subreddit has
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code >= 300:
+    subreddit: subreddit to scrape for
+    """
+    body = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json',
+                        headers={'User-Agent': 'My-Agent'},
+                        allow_redirects=False)
+    if body.status_code >= 300:
         return 0
-    return response.json().get('data').get('subscribers')
+    return body.json().get('data').get('subscribers')
